29214e25413284f442f8a33f9edaeda95bb2e7e6
oracle <oracle@atanasoft29.danielcastelao.org>
2015-06-11 09:13:13 +0200

    Commit con la clase Superficie (controlador). Es el main de la aplicación. Hice un switch case con cada caso: cuadrado, triángulo, círculo, rectángulo...

--- src/superficies/Superficies.java
+++ src/superficies/Superficies.java
@@ -1,6 +1,7 @@
 package superficies;
 
 import java.util.Scanner;
+import static sun.util.calendar.CalendarUtils.mod;
 
 /**
  * Examen de MVC 3ª Avaliación
@@ -9,71 +10,61 @@ import java.util.Scanner;
  */
 public class Superficies {
 
-    private static Scanner sc;
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        String shapeType;
 
-        sc = new Scanner(System.in);
+        Modelo mod = new Modelo();
+        Vista.askType(mod);
 
-        System.out.println("What shape would you like to find the area of? (square, circle, triangle, rectangle):");
-        shapeType = sc.nextLine();
-        System.out.println("You said: " + shapeType);
+        Superficies.shapeType(mod);
+    }
 
-        if (shapeType.equals("square")) {
-            //add area calculations for square
-            float squareArea;
-            float sideLength;
+    public static void shapeType(Modelo mod) {
+        String shapeType = mod.getShapeType();
+        float area;
 
-            System.out.println("what is the side length?");
-            sideLength = sc.nextFloat();
-            squareArea = sideLength * sideLength;
-            System.out.println("The area for your square: " + squareArea);
+        switch (shapeType.toLowerCase()) {
 
-        }
-        if (shapeType.equals("rectangle")) {
-            //add area calculations for rectangle here
-            float sideLength;
-            float sideHeight;
-            float rectangleArea;
+            case "square":
+                Vista.sideLenght(mod);
 
-            System.out.println("what is the rectangles width?");
-            sideLength = sc.nextFloat();
-            System.out.println("What is the rectangles height?");
-            sideHeight = sc.nextFloat();
-            rectangleArea = sideLength * sideHeight;
-            System.out.println("The area for your rectangle is: " + rectangleArea);
+                area = (float) Math.pow(mod.getLength(), mod.getLength());
+                mod.setArea(area);
 
-        }
-        if (shapeType.equals("triangle")) {
-            //add area calculations for triangle here
-            float baseLength;
-            float height;
-            float triangleArea;
+                Vista.showArea(mod);
+                break;
 
-            System.out.println("What is the base length of the triangle?");
-            baseLength = sc.nextFloat();
-            System.out.println("What is the height of the triangle?");
-            height = sc.nextFloat();
-            triangleArea = (float) (0.5 * baseLength * height);
-            System.out.println("Your triangles area is: " + triangleArea);
+            case "rectangle":
+                Vista.askWidth(mod);
+                Vista.askRHeight(mod);
 
-        }
-        if (shapeType.equals("circle")) {
-            //add area calculations for a circle here
-            float radius;
-            float circleArea;
+                area = mod.getLength() * mod.getHeight();
+                mod.setArea(area);
 
-            System.out.println("What is the radius of the circle?");
-            radius = sc.nextFloat();
-            circleArea = radius * radius;
-            circleArea = (float) (3.14159265 * circleArea);
-            System.out.println("Your Circles area is " + circleArea);
+                Vista.showArea(mod);
+                break;
+
+            case "triangle":
+                Vista.askBase(mod);
+                Vista.askHeight(mod);
+
+                area = (float) (0.5 * mod.getLength() * mod.getHeight());
+                mod.setArea(area);
+
+                Vista.showArea(mod);
+                break;
+
+            case "circle":
+                Vista.askRadius(mod);
+
+                area = mod.getRadius() * mod.getRadius();
+                area = (float) (3.14159265 * area);
+                mod.setArea(area);
+
+                Vista.showArea(mod);
+                break;
 
         }
 
     }
+
 }
