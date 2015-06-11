c6db1026e64a66c72cdcd1f4e55ea26414db435a
oracle <oracle@atanasoft29.danielcastelao.org>
2015-06-11 09:39:32 +0200

    Commit con el test recien hecho dandole a Alt + F6 y metiéndole datos para testearlo.

---
+++ ExamenCODTest/test/CalculoTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import examencodtest.Calculo;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author oracle
+ */
+public class CalculoTest {
+    
+    public CalculoTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+
+    /**
+     * Test of obtenerMCD method, of class Calculo.
+     */
+    @Test
+    public void testObtenerMCD() {
+        
+        System.out.println("obtenerMCD");
+        int num1 = 8;
+        int num2 = 4;
+        Calculo instance = new Calculo();
+        int expResult = 12;
+        int result = instance.obtenerMCD(num1, num2);
+        assertEquals(expResult, result);
+        
+        
+    }
+    
+}
